d2d6e297f4b70ff2f4e3de00436e6f6f92a57131
camilotorres199810 <kamilo98@outlook.com>
2020-06-17 19:31:37 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+//Primer punto, Calculadora aritmetica.
+   /* if ($_POST ["valor1"] !="" and $_POST ["valor2"]!=""){
+		if ($_POST["operador"] == "suma") {
+			print ($resultado = $_POST ["valor1"] + $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "resta") {
+			print ($resultado = $_POST ["valor1"] - $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "multiplicacion") {
+			print ($resultado = $_POST ["valor1"] * $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		} elseif ($_POST["operador"] == "division") {
+			print ($resultado = $_POST ["valor1"] / $_POST ["valor2"]);
+			print ('<br /><a href="calculadora.php">Volver</a>');
+		}
+	} else {
+		print("Ingresa alg&uacute;n valor");
+		print ('<br /><a href="calculadora.php">Volver</a>');
+	}
+ 
+	
+
+}*/ 
+
+
+
+
+/*Segundo punto, IMC,El gimnasio Bodytech, lo contrata para desarrollar una aplicación web que permita a sus usuarios calcular el índice de masa corporal,
+basado en la formula 𝐈𝐌𝐂 =
+𝑷𝒆𝒔𝒐
+𝑨𝒍𝒕𝒖𝒓𝒂𝒙𝑨𝒍𝒕𝒖𝒓𝒂
+La clasificación del IMC, es la siguiente:
+• IMC <= 18,4 (Insuficiencia de peso)
+• 18,5<=IMC<=24,9 (Peso normal)
+• 25<=IMC<=29,9 (Sobrepeso)
+• 30<=IMC<=34,9 (Obesidad 1)
+• 35<=IMC<=39,9 (Obesidad 2)
+• IMC>=40 (Obesidad 3)*/
+
+/*$peso=40;
+$altura=1.60;
+
+$imc= $peso / ($altura * $altura);
+
+if( $imc<= 18.4){
+
+    echo("Insuficiencia de peso");
+}
+else if ($imc >= 18.5 && $imc<=24.9) {
+   
+    echo("Peso normal");
+
+}
+
+else if ($imc >= 25 && $imc<=29.9){
+
+    echo("Sobrepeso");
+
+}
+
+else if($imc >= 30 && $imc<=34.9){
+ 
+    echo("Obesidad 1");
+
+}
+
+else if($imc >= 35 && $imc<=39.9){
+
+  echo("Obesidad2");
+
+}
+
+else if($imc>=40){
+
+    echo("Obesidad 3");
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+?> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
